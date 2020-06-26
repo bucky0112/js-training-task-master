cdd895692cb973d4cd8cb683d870193a3f57142e
bucky0112 <bucky0112@gmail.com>
2020-06-26 19:21:55 +0800

    done

--- week1/all.js
+++ week1/all.js
@@ -1,90 +1,79 @@
-var todoData = [];
+const inputText = document.querySelector(".input");
+const add = document.querySelector(".add");
+const list = document.querySelector(".list");
+const deleteBtn = document.querySelector("ul");
+const listNum = document.querySelector(".listNum");
+const clean = document.querySelector(".cleanBTN");
 
-      document.getElementById('addTodo').addEventListener('click', function () {
-        var str = '';
-        if (document.getElementById('newTodo').value.trim() !== '') {
-          todoData.push({
-            id: Math.floor(Date.now()),
-            title: document.getElementById('newTodo').value,
-            completed: false,
-          })
-          todoData.forEach(function (item) {
-            str += `<li class="list-group-item">
-<div class="d-flex">
-<div class="form-check">
-<input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
-<label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
-</div>
-<button type="button" class="close ml-auto" aria-label="Close">
-<span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
-</button>
-</div>
-</li>`;
-          })
-          document.getElementById('todoList').innerHTML = str;
-          document.getElementById('taskCount').textContent = todoData.length;
-          document.getElementById('newTodo').value = '';
+const listData = [];
+add.addEventListener("click", addList);
+clean.addEventListener("click", clearAll);
+deleteBtn.addEventListener("click", removeList);
+deleteBtn.addEventListener("click", status);
+
+function addList() {
+  const value = inputText.value.trim();
+  // console.log(value);
+  if (!value) {
+    return;
+  } else {
+    listData.push({
+      text: value,
+      status: false,
+      id: listData.length
+    });
+  }
+  // console.log(listData);
+  render();
+  inputText.value = "";
+}
+
+function render() {
+  let str = "";
+  listData.forEach((item, i) => {
+    str += `
+            <li>
+                <label class='checkbox ${item.status ? "done" : ""}'>
+                    <input type='checkbox' class='status' ${
+                    item.status ? "checked" : ""
+                    } data-num=${i}>
+                    ${item.text}
+                </label>
+                <a href="" class='delete' data-num='${i}'>刪除</a>
+            </li>
+        `;
+  });
+  list.innerHTML = str;
+  listNum.innerHTML = `目前有 ${listData.length} 筆任務`
+}
+
+function clearAll() {
+    e.preventDefault();
+    listData = [];
+    render()
+}
+
+function removeList(e) {
+  e.preventDefault();
+  // console.log(e.target.nodeName);
+  if (e.target.nodeName === "A") {
+    listData.splice(e.target.dataset.num, 1);
+  }
+  render();
+}
+
+function status(e) {
+  // console.log(e.target.nodeName);
+  if (e.target.nodeName === "INPUT") {
+    listData.forEach((item) => {
+      if (e.target.dataset.num == item.id) {
+        if (item.status) {
+          item.status = false;
+        } else {
+          item.status = true;
         }
-      });
-      document.getElementById('clearTask').addEventListener('click', function (e) {
-        e.preventDefault();
-        todoData = [];
-        document.getElementById('todoList').innerHTML = '';
-        document.getElementById('taskCount').textContent = todoData.length;
-      });
-      document.getElementById('todoList').addEventListener('click', function (e) {
-        var newIndex = 0;
-        if (e.target.dataset.action == 'remove') {
-          todoData.forEach(function (item, key) {
-            if (e.target.dataset.id == item.id) {
-              newIndex = key;
-            }
-          })
-          todoData.splice(newIndex, 1);
-          var str = '';
-          todoData.forEach(function (item) {
-            str += `<li class="list-group-item">
-<div class="d-flex">
-<div class="form-check">
-<input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
-<label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
-</div>
-<button type="button" class="close ml-auto" aria-label="Close">
-<span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
-</button>
-</div>
-</li>`;
-          });
-          document.getElementById('todoList').innerHTML = str;
-          document.getElementById('taskCount').textContent = todoData.length;
-        }
-      });
-      document.getElementById('todoList').addEventListener('click', function (e) {
-        if (e.target.dataset.action == 'complete') {
-          todoData.forEach(function (item) {
-            if (e.target.dataset.id == item.id) {
-              if (item.completed) {
-                item.completed = false;
-              } else {
-                item.completed = true;
-              }
-            }
-          })
-          var str = '';
-          todoData.forEach(function (item) {
-            str += `<li class="list-group-item">
-<div class="d-flex">
-<div class="form-check">
-<input type="checkbox" class="form-check-input" ${item.completed ? 'checked' : ''} data-action="complete" data-id="${item.id}">
-<label class="form-check-label ${item.completed ? 'completed' : ''}" data-action="complete" data-id="${item.id}"> ${item.title}</label>
-</div>
-<button type="button" class="close ml-auto" aria-label="Close">
-<span aria-hidden="true" data-action="remove" data-id="${item.id}">&times;</span>
-</button>
-</div>
-</li>`;
-          })
-          document.getElementById('todoList').innerHTML = str;
-          document.getElementById('taskCount').textContent = todoData.length;
-        }
-      });
+      }
+    });
+    render();
+  }
+}
